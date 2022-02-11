1189e47c328d3b71cce6f43102c7d5827a0f9503
kimnew6 <kimnew6@gmail.com>
2022-02-11 16:16:59 +0900

    add: useNetwork

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './useFadeIn';
+import App from './useNetwork';
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ react-hooks/src/useNetwork.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+
+const useNetwork = onChange => {
+  const [status, setStatus] = useState(navigator.onLine);
+  const handleChange = () => {
+    if (typeof onChange === 'function') {
+      onChange(navigator.onLine);
+    }
+    setStatus(navigator.onLine);
+  };
+  useEffect(() => {
+    window.addEventListener('online', handleChange);
+    window.addEventListener('offline', handleChange);
+    // eslint-disable-next-line no-unused-expressions
+    () => {
+      window.removeEventListener('online', handleChange);
+      window.removeEventListener('offline', handleChange);
+    };
+  }, []);
+  return status;
+};
+
+const App = () => {
+  const handleNetworkChange = online => {
+    console.log(online ? 'We just went online' : 'We are offline');
+  };
+  const onLine = useNetwork(handleNetworkChange);
+  return (
+    <div>
+      <h1>{onLine ? 'Online' : 'Offline'}</h1>
+    </div>
+  );
+};
+
+export default App;
