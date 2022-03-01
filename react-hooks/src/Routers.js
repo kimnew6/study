0d61c04abe7be531a07aab532cd1146569b9d69f
kimnew6 <kimnew6@gmail.com>
2022-03-02 06:36:50 +0900

    add: Router

---
+++ react-hooks/src/Routers.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import UseAxios from './pages/useAxios';
+import UseBeforeLeave from './pages/useBeforeLeave';
+import UseClick from './pages/useClick';
+import UseEffect from './pages/useEffect';
+import UseFadeIn from './pages/useFadeIn';
+import UseFullscreen from './pages/useFullscreen';
+import UseHover from './pages/useHover';
+import UseInput from './pages/useInput';
+import UseNetwork from './pages/useNetwork';
+import UseNotification from './pages/useNotification';
+import UsePreventLeave from './pages/usePreventLeave';
+import UseScroll from './pages/useScroll';
+import UseState from './pages/useState';
+import UseTabs from './pages/useTabs';
+import UseTitle from './pages/useTitle';
+
+const Routers = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/useaxios" element={<UseAxios />} />
+        <Route path="/usebeforeLeave" element={<UseBeforeLeave />} />
+        <Route path="/useclick" element={<UseClick />} />
+        <Route path="/useeffect" element={<UseEffect />} />
+        <Route path="/usefadein" element={<UseFadeIn />} />
+        <Route path="/usefullscreen" element={<UseFullscreen />} />
+        <Route path="/usehover" element={<UseHover />} />
+        <Route path="/useinput" element={<UseInput />} />
+        <Route path="/usenetwork" element={<UseNetwork />} />
+        <Route path="/usenotification" element={<UseNotification />} />
+        <Route path="/usepreventleave" element={<UsePreventLeave />} />
+        <Route path="/usescroll" element={<UseScroll />} />
+        <Route path="/usestate" element={<UseState />} />
+        <Route path="/usetabs" element={<UseTabs />} />
+        <Route path="/usetitle" element={<UseTitle />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Routers;
--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './useAxios';
+import Routers from './Routers';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routers />
   </React.StrictMode>,
   document.getElementById('root')
 );
