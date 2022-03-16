4fe114084325e87cec3eee27a67c962c4dec7db5
kimnew6 <kimnew6@gmail.com>
2022-03-16 12:45:40 +0900

    add: mapStateToProps

--- redux/src/routes/Home.js
+++ redux/src/routes/Home.js
@@ -1,1 +1,30 @@
-export default () => 'Home';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+
+function Home({ toDos }) {
+  const [text, setText] = useState('');
+  function onChange(e) {
+    setText(e.target.value);
+  }
+  function onSubmit(e) {
+    e.preventDefault();
+    console.log(text);
+  }
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} />
+        <button>Add</button>
+      </form>
+      <ul>{JSON.stringify(toDos)}</ul>
+    </>
+  );
+}
+
+//Redux state로부터 home(component)에 prop으로 전달.
+function mapStateToProps(state) {
+  return { toDos: state }; //toDos는 여기서 만들어 준 것.
+}
+
+export default connect(mapStateToProps)(Home);
