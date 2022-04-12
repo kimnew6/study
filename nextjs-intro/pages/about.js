22c1c44d912d14f997eef389e642c3a9860c07e9
kimnew6 <kimnew6@gmail.com>
2022-04-12 23:07:25 +0900

    add: link, router

--- nextjs-intro/pages/about.js
+++ nextjs-intro/pages/about.js
@@ -1,6 +1,9 @@
+import NavBar from "../components/NavBar";
+
 export default function frontworker() {
   return (
     <div>
+      <NavBar />
       <h1>Hello gravision</h1>
       <h2>front worker</h2>
     </div>
